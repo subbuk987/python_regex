fa36bc7bb0bab650a81c3b1f9199bb6c58b95cb2
Subramanyam Kandlagunta <subramanyamk2003@gmail.com>
2025-04-12 00:03:17 +0530

    add PassWord Check 1

--- UserRegistration/registration.py
+++ UserRegistration/registration.py
@@ -8,6 +8,7 @@ class User:
     _NAME_PATTERN = re.compile(r'\b[A-Z][A-Za-z]{2,}\b')
     _EMAIL_PATTERN = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b')
     _MOBILE_PATTERN = re.compile(r'\b[1-9][0-9]{0,2}\s[0-9]{10}\b')
+    _PASSWORD_PATTERN = re.compile(r'^.{8,20}$') # Minimum 8 Characters
 
     def __init__(self):
         self.first_name = None
@@ -43,11 +44,20 @@ class User:
             else:
                 print("Entered Mobile Number is NOT Valid!!!")
 
+    def _accept_password(self):
+        while True:
+            password = input("Enter Password: ")
+            if self._PASSWORD_PATTERN.fullmatch(password):
+                return password
+            else:
+                print("Password Should be At Least 8 Characters!!!")
+
     def create(self):
         self.first_name = self._accept_name("First Name")
         self.last_name = self._accept_name("Last Name")
         self._email = self._accept_email()
         self._mobile_number = self._accept_mobile()
+        self._password = self._accept_password()
 
 
 if __name__ == "__main__":
