23709e9e6a30169e3002d43e0cd7622d12bb87ee
Subramanyam Kandlagunta <subramanyamk2003@gmail.com>
2025-04-11 23:26:08 +0530

    add Email Check

--- UserRegistration/registration.py
+++ UserRegistration/registration.py
@@ -6,6 +6,7 @@ from exceptions import FormatError
 
 class User:
     _NAME_PATTERN = re.compile(r'\b[A-Z][A-Za-z]{2,}\b')
+    _EMAIL_PATTERN = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b')
 
     def __init__(self):
         self.first_name = None
@@ -25,9 +26,18 @@ class User:
                     f"contain at least 3 characters"
                 )
 
+    def _accept_email(self):
+        while True:
+            email = input("Enter Email Address: ")
+            if self._EMAIL_PATTERN.fullmatch(email):
+                return email
+            else:
+                print("Email Address is not valid!!!")
+
     def create(self):
         self.first_name = self._accept_name("First Name")
         self.last_name = self._accept_name("Last Name")
+        self._email = self._accept_email()
 
 
 if __name__ == "__main__":
